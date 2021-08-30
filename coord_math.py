0bc9cab3ccc7e37000ccc7c8867322f0c25dad6c
Matthew Riche <matthew.riche@gmail.com>
2021-08-30 17:08:53 -0400

    Moved the aim_at module to transforms module

--- coord_math.py
+++ coord_math.py
@@ -4,6 +4,7 @@
 
 
 import pymel.core as pm
+import pymel.core.datatypes as dt
 import maya.api.OpenMaya as om
 
 
@@ -18,6 +19,12 @@ def get_component_ID(component):
 
     print ("Component ID was {}".format(id_element))
     return id_element
+
+
+def get_point_at_range(source_point, point):
+    '''
+    Given a point 
+    '''
     
 
 def get_average_xform(nodes):
@@ -67,9 +74,9 @@ def get_normal(point_a, point_b, point_c):
     '''
 
     # Turn coord lists into vectors so the math is easy:
-    vec_a = np.array(point_a)
-    vec_b = np.array(point_b)
-    vec_c = np.array(point_c)
+    vec_a = dt.Vector(point_a)
+    vec_b = dt.Vector(point_b)
+    vec_c = dt.Vector(point_c)
 
     normal = (vec_c - vec_a) * (vec_b - vec_a)
 
@@ -86,32 +93,12 @@ def get_vector(point_a, point_b):
     '''
 
     # Grab numpy vectors for after
-    vpoint_a = np.array(point_a)
-    vpoint_b = np.array(point_b)
+    vpoint_a = dt.Vector(point_a)
+    vpoint_b = dt.Vector(point_b)
     vector = vpoint_a - vpoint_b
 
     return vector
 
-
-def get_unit_vector(vector):
-    '''
-    given two points, normalize the line into a unit vector.
-    The points are non-numpy lists.
-
-    usage:
-    get_unit_vector(point_a=[lists], point_b=[lists])
-    '''
-    point_a = vector[0]
-    point_b = vector[1]
-
-    # Calculate magnitude
-    magnitude = np.sqrt(point_a[0]**2 + point_a[1]**2 + point_a[2]**2)
-
-    # Normalize into unit vector.
-    normalized_mag = vector / magnitude
-
-    return normalized_mag
-    
 
 def match_xform(target_node, subject_node, rotate=True):
     '''
