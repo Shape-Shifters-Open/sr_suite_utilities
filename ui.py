644cc4ec57b83289b804b356c781a83af136d3e8
Anoshka Jhaveri <93942821+Anoshka@users.noreply.github.com>
2021-12-10 21:05:24 -0800

    added 2 more buttons and transforms tab
    - created transforms tab
    - added copy orient buttons

--- ui.py
+++ ui.py
@@ -9,18 +9,20 @@ import sys
 from PySide2 import QtCore, QtWidgets, QtGui
 from shiboken2 import wrapInstance
 import gc
+import sys
 
 # From related modules:
+
 import globals
 import skeleton
 import skinning
 import handles
 import connections
-import orientation
 import dict_lib
 import all_control_options
-
+import orientation
 from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
+
 
 def maya_main_window():
     main_window_ptr = omui.MQtUtil.mainWindow()
@@ -28,7 +30,7 @@ def maya_main_window():
 
 
 class MainDialog(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
-    
+
     def __init__(self, parent=maya_main_window()):
         super(MainDialog, self).__init__(parent)
         self.setWindowTitle("Shaper Rigs Suite Utilities v{}".format(globals.srsu_version))
@@ -42,9 +44,7 @@ class MainDialog(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         self.create_layouts()
         self.create_connections()
 
-        
     def create_widgets(self):
-        
         # Prep tabs:
         self.tools_tab = QtWidgets.QTabWidget(self)
         self.tools_tab.setGeometry(QtCore.QRect(20, 20, 371, 361))
@@ -56,13 +56,19 @@ class MainDialog(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         self.tools_tab.addTab(self.skeleton_tab, "Skeleton")
         self.jfc_btn = QtWidgets.QPushButton(self.skeleton_tab)
         self.jfc_btn.setText("Joint from Components")
+
+        #      adding dumb copy orient
+        self.dumb_copy_orient_btn = QtWidgets.QPushButton(self.skeleton_tab)
+        self.dumb_copy_orient_btn.setText("dumb copy orient")
+
+        #      adding smart copy orient
         self.smart_copy_orient_btn = QtWidgets.QPushButton(self.skeleton_tab)
-        self.smart_copy_orient_btn.setText("Smart Copy Orientation")
+        self.smart_copy_orient_btn.setText("smart copy orient")
 
         # Skinning Tab:
         self.skin_tab = QtWidgets.QWidget()
         self.tools_tab.addTab(self.skin_tab, "skinning")
-        
+
         self.copy_sw_btn = QtWidgets.QPushButton(self.skin_tab)
         self.copy_sw_btn.setText("Copy SkinWeights")
 
@@ -91,7 +97,7 @@ class MainDialog(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         self.h_connect_xforms_btn = QtWidgets.QPushButton(self.connections_tab)
         self.h_connect_xforms_btn.setText("Connect Xform")
 
-        #     add driver attribute prompt and textbox
+        #     add driver attribute prompt  and textbox
         self.h_input_driver_txtbox = QtWidgets.QLabel(self.connections_tab)
         self.h_input_driver_txtbox.setText("Input Driver Attribute")
         self.get_driver = QtWidgets.QLineEdit(self.connections_tab)
@@ -114,12 +120,7 @@ class MainDialog(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         self.tools_tab.addTab(self.controls_tab, "Controls")
         self.h_swap_control_btn = QtWidgets.QPushButton(self.controls_tab)
         self.h_swap_control_btn.setText("Swap Shape")
-        #self.h_swap_control_btn.resize(370, 20)
-
-        # Transforms tab
-        self.trans_tab = QtWidgets.QWidget()
-        self.tools_tab.addTab(self.trans_tab, "Transforms")
-
+        # self.h_swap_control_btn.resize(370, 20)
 
         self.shape_options = QtWidgets.QComboBox(self.controls_tab)
         self.shape_options.move(10, 81)
@@ -140,21 +141,75 @@ class MainDialog(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         self.h_color_options_btn = QtWidgets.QPushButton(self.controls_tab)
         self.h_color_options_btn.setText("Colour Options")
 
-
-
         # FKIK Ttab
         self.fkik_tab = QtWidgets.QWidget()
         self.tools_tab.addTab(self.fkik_tab, "FK/IK")
 
-
         self.tools_tab.setCurrentIndex(0)
 
+        # Transforms tab
+        self.transforms_tab = QtWidgets.QWidget()
+        self.tools_tab.addTab(self.transforms_tab, "Transforms")
+        self.h_aim_at_btn = QtWidgets.QPushButton(self.transforms_tab)
+        self.h_aim_at_btn.setText("Aim At")
+        self.h_aim_at_btn.move(0, 36)
+        self.h_aim_at_btn.resize(40, 20)
+
+        #     adding pole vector label
+        self.h_input_pv_label = QtWidgets.QLabel(self.transforms_tab)
+        self.h_input_pv_label.setText("Pole Vector")
+        self.h_input_pv_label.move(45, 36)
+        self.h_input_pv_label.resize(80, 20)
+
+        #     adding float input dialog
+        self.h_input_pv_x = QtWidgets.QLineEdit(self.transforms_tab)
+        self.h_input_pv_x.move(110, 36)
+        self.h_input_pv_x.resize(30, 20)
+        self.h_input_pv_x.setText("0.0")
+        self.h_input_pv_y = QtWidgets.QLineEdit(self.transforms_tab)
+        self.input_y = self.h_input_pv_y.text()
+        self.h_input_pv_y.move(140, 36)
+        self.h_input_pv_y.resize(30, 20)
+        self.h_input_pv_y.setText("0.1")
+        self.h_input_pv_z = QtWidgets.QLineEdit(self.transforms_tab)
+        self.input_z = self.h_input_pv_z.text()
+        self.h_input_pv_z.move(170, 36)
+        self.h_input_pv_z.resize(30, 20)
+        self.h_input_pv_z.setText("0.0")
+
+        #     adding vector combobox label
+        self.h_vector_label = QtWidgets.QLabel(self.transforms_tab)
+        self.h_vector_label.setText("Vector")
+        self.h_vector_label.move(205, 36)
+        self.h_vector_label.resize(80, 20)
+
+        #     adding vector combobox
+        self.vector_enum = QtWidgets.QComboBox(self.transforms_tab)
+        self.vector_enum.move(240, 36)
+        self.vector_enum.resize(40, 20)
+        self.vector_enum.addItem('x')
+        self.vector_enum.addItem('y')
+        self.vector_enum.addItem('z')
+
+        #     adding up vector label
+        self.h_up_vector_label = QtWidgets.QLabel(self.transforms_tab)
+        self.h_up_vector_label.setText("Up Vector")
+        self.h_up_vector_label.move(280, 36)
+        self.h_up_vector_label.resize(80, 20)
+
+        #     adding up vector combobox
+        self.up_vector_enum = QtWidgets.QComboBox(self.transforms_tab)
+        self.up_vector_enum.move(330, 36)
+        self.up_vector_enum.resize(40, 20)
+        self.up_vector_enum.addItem('x')
+        self.up_vector_enum.addItem('y')
+        self.up_vector_enum.addItem('z')
 
     def create_layouts(self):
-
         # Create the skeleton tab layout:
         skeleton_tab_layout = QtWidgets.QFormLayout(self.skeleton_tab)
         skeleton_tab_layout.addRow(self.jfc_btn)
+        skeleton_tab_layout.addRow(self.dumb_copy_orient_btn)
         skeleton_tab_layout.addRow(self.smart_copy_orient_btn)
 
         # Create the skinning tab layout:
@@ -181,15 +236,11 @@ class MainDialog(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         controls_tab_layout.addRow(self.h_color_options_btn)
         controls_tab_layout.addRow(self.h_swap_control_btn)
 
-
-
-
-
-
+        # Create Transforms Tab layout:
+        transforms_tab_layout = QtWidgets.QFormLayout(self.transforms_tab)
 
     def create_connections(self):
         self.jfc_btn.clicked.connect(self.ui_joint_from_component)
-        self.smart_copy_orient_btn.clicked.connect(self.ui_smart_copy_orient)
         self.harden_btn.clicked.connect(self.ui_harden_skin)
         self.copy_sw_btn.clicked.connect(self.ui_copy_skinweights)
         self.find_rs_btn.clicked.connect(self.ui_find_related_cluster)
@@ -203,6 +254,9 @@ class MainDialog(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         self.shape_options.currentText()
         self.h_control_selection_btn.clicked.connect(self.ui_control_options)
         self.h_color_options_btn.clicked.connect(self.ui_colour_options)
+        self.h_aim_at_btn.clicked.connect(self.ui_aim_at)
+        self.dumb_copy_orient_btn.clicked.connect(self.ui_dumb_copy_orient)
+        self.smart_copy_orient_btn.clicked.connect(self.ui_smart_copy_orient)
 
 
     # UI commands:
@@ -210,10 +264,6 @@ class MainDialog(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
     def ui_joint_from_component(self):
         print ("UI call for skeleton.joint_from_components()")
         skeleton.joint_from_components()
-
-    def ui_smart_copy_orient(self):
-        print("UI call to orientation.smart_copy_orient()")
-        orientation.smart_copy_orient()
 
     def ui_harden_skin(self):
         print ("UI call for skinning.harden()")
@@ -241,7 +291,7 @@ class MainDialog(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
 
     def ui_batch_connect(self):
         print ("UI call for connections.batch_connect()")
-        #uses input attributes for parameters
+        # uses input attributes for parameters
         connections.batch_connect(str(self.get_driver.text()), str(self.get_driven.text()))
 
     def ui_swap_controls(self):
@@ -256,8 +306,31 @@ class MainDialog(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         print ("UI call for picking colour")
         all_control_options.pick_colour()
 
+    def ui_aim_at(self):
+        print ("UI call for aim at button")
 
-    
+        # takes selection as node and target
+        nodes = pm.ls(selection=True)
+        node = nodes[0]
+        target = nodes[1]
+
+        # takes values entered
+        pv_x = float(self.h_input_pv_x.text())
+        pv_y = float(self.h_input_pv_y.text())
+        pv_z = float(self.h_input_pv_z.text())
+
+        orientation.aim_at(node, target, vec=self.vector_enum.currentText(),
+                           pole_vec=(pv_x, pv_y, pv_x),
+                           axis=self.up_vector_enum.currentIndex(), pole=2)
+
+
+    def ui_dumb_copy_orient(self):
+        orientation.dumb_copy_orient()
+
+
+    def ui_smart_copy_orient(self):
+        orientation.smart_copy_orient()
+
 def run():
     """displays windows"""
 
